bec63c794ad5787d4b2dff8da9a4034b49b7a640
Jeremy DEJOUX <jeremy.dejoux@laplateforme.io>
2022-07-04 01:21:45 +0200

    end of the days

--- game.py
+++ game.py
@@ -1,22 +1,31 @@
-import imp
-
-
 import pygame
+import pytmx
+import pyscroll
 
 class Game:
 
     def __init__(self):
         #creer la fenetre du jeu
-        pygame.display.set_mode((800, 600)) #definit la taille de la fenetre en px
+        self.screen = pygame.display.set_mode((800, 600)) #definit la taille de la fenetre en px
         pygame.display.set_caption("Labyrinthe") #definit le titre de la fenetre
+
+        #charger la carte au format TMX
+        tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+
+        #dessiner le groupe de calques
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
 
     def run(self):
         #Boucle de jeux permettant de maintenir la fenetre ouverte
         running = True
 
         while running:
+            self.group.draw(self.screen)
+            pygame.display.flip() #Permet de actualiser à chaque tour de boucle
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: #Si on appuie sur la croix pour quitter la page
                     running = False
-                    
+
         pygame.quit()
