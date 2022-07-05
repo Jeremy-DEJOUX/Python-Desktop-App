7d5e22b8504246a6750c9240383f2dabfec258e4
Jeremy DEJOUX <jeremy.dejoux@laplateforme.io>
2022-07-05 10:59:33 +0200

    Deplacement du joeur finit

--- game.py
+++ game.py
@@ -18,22 +18,42 @@ class Game:
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
 
         # generer un jouer
-        self.player = Player(30, 40)
+        player_position = tmx_data.get_object_by_name("player")
+        self.player = Player(player_position.x, player_position.y)
 
         # dessiner le groupe de calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
         self.group.add(self.player)
 
+    def handle_input(self):
+        pressed = pygame.key.get_pressed() #recupère les touches pressé
+
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+
     def run(self):
+
+        clock = pygame.time.Clock()
+
         # Boucle de jeux permettant de maintenir la fenetre ouverte
         running = True
 
         while running:
+            self.handle_input()
             self.group.update()
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()  # Permet de actualiser à chaque tour de boucle
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:  # Si on appuie sur la croix pour quitter la page
                     running = False
+
+            clock.tick(60)
 
         pygame.quit()
